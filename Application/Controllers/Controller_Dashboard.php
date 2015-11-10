dad27b2492de65287f1b21ce515ba4cbf695e63b
Darevski Victor <vitik998@gmail.com>
2015-11-11 02:21:22 +0300

    BackEnd Добавление функций по выводу Списка Групп, актуального рассписания сегодня/завтра

--- Application/Controllers/Controller_Dashboard.php
+++ Application/Controllers/Controller_Dashboard.php
@@ -19,4 +19,29 @@ class Controller_Dashboard extends Core\Controller
     {
         $this->view->generate();
     }
+
+    /**
+     * Получение списка всех групп (курса группы) и вывод в виде json строки
+     */
+    function action_get_list_group(){
+        $list_group=$this->model->get_list_group();
+        $list_group=json_encode($list_group);
+        $this->view->output_json($list_group);
+    }
+
+    /**
+     * Получение рассписания на сегодня/следующий учебный день для указанной группы
+     * и вывод в виде json строки
+     */
+    function action_actual_dashboard(){
+        $_POST['group_number']='32494';
+        if (isset($_POST['group_number'])){
+            $group_number = $this->security_variable($_POST['group_number']);
+
+            $dashboard = $this->model->get_actual_dashboard($group_number);
+            $dashboard = json_encode($dashboard);
+            $this->view->output_json($dashboard);
+        }
+
+    }
 }
